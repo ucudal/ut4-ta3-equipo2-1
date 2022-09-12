c974be331b36e23376aaadedd8f018d81d4f3a72
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2022-09-12 19:41:17 -0300

    Integro

--- TA3 - Alumnos/test/TArbolBBTest.java
+++ TA3 - Alumnos/test/TArbolBBTest.java
@@ -35,13 +35,59 @@ public class TArbolBBTest {
         assertEquals(expResult, result);
     }
     @Test
-    public void testObtenerTamanioVarios() {
+    public void testObtenerTamanioDer() {
         System.out.println("obtenerTamanio");
         TArbolBB instance = new TArbolBB();
         instance.insertar(new TElementoAB(0,2));
         instance.insertar(new TElementoAB(1,22));
         instance.insertar(new TElementoAB(22,22));
         int expResult = 3;
+        int result = instance.obtenerTamanio();
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testObtenerTamanioIzq() {
+        System.out.println("obtenerTamanio");
+        TArbolBB instance = new TArbolBB();
+        instance.insertar(new TElementoAB(10,10));
+        instance.insertar(new TElementoAB(9,9));
+        instance.insertar(new TElementoAB(8,8));
+        int expResult = 3;
+        int result = instance.obtenerTamanio();
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testObtenerTamanioDerIzq() {
+        System.out.println("obtenerTamanio");
+        TArbolBB instance = new TArbolBB();
+        instance.insertar(new TElementoAB(10,10));
+        instance.insertar(new TElementoAB(11,11));
+        instance.insertar(new TElementoAB(9,9));
+        int expResult = 3;
+        int result = instance.obtenerTamanio();
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testObtenerTamanioDerIzqIzq() {
+        System.out.println("obtenerTamanio");
+        TArbolBB instance = new TArbolBB();
+        instance.insertar(new TElementoAB(10,10));
+        instance.insertar(new TElementoAB(11,11));
+        instance.insertar(new TElementoAB(9,9));
+        instance.insertar(new TElementoAB(3,3));
+        int expResult = 4;
+        int result = instance.obtenerTamanio();
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testObtenerTamanioDerIzqDer() {
+        System.out.println("obtenerTamanio");
+        TArbolBB instance = new TArbolBB();
+        instance.insertar(new TElementoAB(10,10));
+        instance.insertar(new TElementoAB(11,11));
+        instance.insertar(new TElementoAB(9,9));
+        instance.insertar(new TElementoAB(22,22));
+        int expResult = 4;
         int result = instance.obtenerTamanio();
         assertEquals(expResult, result);
     }
